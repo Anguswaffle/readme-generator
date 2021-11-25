8dbb0755f9d56582300c129a8f24c99d48d605d1
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-11-25 12:26:04 -0700

    Finished license section

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project?'
+  },
+  {
+    type: 'confirm',
+    name: 'wants_license',
+    message: 'Do you need a license?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What license do you need?',
+    choices: ['GNU AGPL v3', 'GNU GPL v3', 'GNU LGPL v3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+    default: 'MIT License',
+    when: (answers) => answers.wants_license === true
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const markdownStr = generateMarkdown(data)
+  fs.writeFile(`./output/README-${fileName}.md`, markdownStr, (err) =>
+    err ? console.log(err) : console.log(`Successfully created ${fileName}.md!`))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then((answers) => writeToFile(answers.title, answers))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,54 @@
+const licenseLinkArr = ['https://www.gnu.org/licenses/agpl-3.0', 'https://www.gnu.org/licenses/gpl-3.0', 'https://www.gnu.org/licenses/lgpl-3.0', 'https://opensource.org/licenses/MPL-2.0', 'https://opensource.org/licenses/Apache-2.0', 'https://opensource.org/licenses/MIT', 'https://www.boost.org/LICENSE_1_0.txt', 'http://unlicense.org/']
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'GNU AGPL v3': return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](${licenseLinkArr[0]})`;
+    case 'GNU GPL v3': return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](${licenseLinkArr[1]})`;
+    case 'GNU LGPL v3': return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](${licenseLinkArr[2]})`;
+    case 'Mozilla Public License 2.0': return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](${licenseLinkArr[3]})`;
+    case 'Apache License 2.0': return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](${licenseLinkArr[4]})`;
+    case 'MIT License': return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](${licenseLinkArr[5]})`;
+    case 'Boost Software License 1.0': return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](${licenseLinkArr[6]})`;
+    case 'The Unlicense': return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](${licenseLinkArr[7]})`;
+    default: return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'GNU AGPL v3': return `[${license}](${licenseLinkArr[0]})`;
+    case 'GNU GPL v3': return `[${license}](${licenseLinkArr[1]})`;
+    case 'GNU LGPL v3': return `[${license}](${licenseLinkArr[2]})`;
+    case 'Mozilla Public License 2.0': return `[${license}](${licenseLinkArr[3]})`;
+    case 'Apache License 2.0': return `[${license}](${licenseLinkArr[4]})`;
+    case 'MIT License': return `[${license}](${licenseLinkArr[5]})`;
+    case 'Boost Software License 1.0': return `[${license}](${licenseLinkArr[6]})`;
+    case 'The Unlicense': return `[${license}](${licenseLinkArr[7]})`;
+    default: return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) return '';
+
+  return `## License 
+
+  Licensed under ${renderLicenseLink(license)}.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title.toUpperCase()}
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ${renderLicenseSection(data.license)} 
+  `;
 }
 
 module.exports = generateMarkdown;
